83f14394338361b01e1bceff9bf4bd72e20ae681
n-zu <core.editor>
2023-10-29 23:58:11 -0300

    add test

--- containers/manager/src/test/scala/marshalling/WorkPickleTest.scala
+++ containers/manager/src/test/scala/marshalling/WorkPickleTest.scala
@@ -1,7 +1,7 @@
 package org.grid_search.manager
 package marshalling
 
-import work_split.{Interval, Work}
+import work_split.{Interval, Work, Aggregator}
 import org.scalatest.funsuite.AnyFunSuite
 
 class WorkPickleTest extends AnyFunSuite {
@@ -29,6 +29,16 @@ class WorkPickleTest extends AnyFunSuite {
     test("Work pickling works, with precision") {
 
       val baseWork = Work(List(Interval(0, 10, 1, 3), Interval(10, 20, 1, 3)))
+
+      val parsed = upickle.default.write(baseWork)
+      val unparsed = upickle.default.read[Work](parsed)
+
+      assert(unparsed == baseWork)
+    }
+
+    test("Work pickling works, with Aggregator") {
+
+      val baseWork = Work(List(Interval(0, 10, 1), Interval(10, 20, 1)), Aggregator.Min)
 
       val parsed = upickle.default.write(baseWork)
       val unparsed = upickle.default.read[Work](parsed)
